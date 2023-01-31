1674a74e78c9ea25b3acabc98b0b4a23e625d63a
Juan Rivera <indiojuan2012.jr@gmail.com>
2023-01-31 18:59:32 -0300

    task 2

---
+++ assets/js/past-events.js
@@ -0,0 +1,26 @@
+const cards = document.getElementById("container")
+
+let eventInfo = data.events
+let card = " "
+let fechaAnterior = data.currentDate
+
+for (let recInfo of eventInfo){
+    if (`${recInfo.date}` < fechaAnterior){
+      
+        card += ` <div class="card m-2" style="width: 10rem;" id="1">
+        <img src="${recInfo.image}" class="card-img-top"
+            alt=" Festival of the collectivities">
+        <div class="card-body">
+            <h5 class="card-title">${recInfo.name}</h5>
+            <p class="card-text">${recInfo.date} </p>
+    
+            <div class="d-flex justify-content-around ">
+                <p class="card-price">$${recInfo.price}</p>
+                <a href="./descripcion.html" class="btn text-light">More</a>
+            </div>
+    
+        </div>
+    </div>`
+    }
+}
+container.innerHTML += card
