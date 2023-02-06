93977602462b44c21d1df00ddd7f37928062ddd9
Juan Rivera <indiojuan2012.jr@gmail.com>
2023-02-06 18:37:58 -0300

    task 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
-const container = document.getElementById("container");
-const eventInfo = data.events;
 
+const container = document.getElementById("container");
+
+const eventInfo = data.events;
 
 const cards = eventInfo.map(recInfo => {
   return `
-    <div class="card m-2" style="width: 10rem;">
-      <img src="${recInfo.image}" class="card-img-top" alt="Festival of the collectivities">
+    <div class="card m-2" style="width: 13rem;" id="${recInfo.name}" data-type="${recInfo.category}">
+      <img src="${recInfo.image}" class="card-img-top" alt="Festival of the collectivities" style="height: 7rem;">
       <div class="card-body">
         <h5 class="card-title">${recInfo.name}</h5>
         <p class="card-text">${recInfo.date}</p>
@@ -18,5 +19,79 @@ const cards = eventInfo.map(recInfo => {
   `;
 }).join("");
 
+
 container.innerHTML += cards;
+
+
+
+
+//search
+const searchInput = document.querySelector(".form-control");
+searchInput.addEventListener("input", e => {
+  const search = e.target.value.toLowerCase();
+  const cards = document.querySelectorAll(".card");
+
+  let hay = false;
+
+  cards.forEach(card => {
+    const name = card.querySelector(".card-title").innerText.toLowerCase();
+    if (name.startsWith(search)) {
+      hay = true;
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+
+  if (!hay) {
+    container.innerHTML = `<div class="error"> <div class="p-reset"><p class="text-center">Sorry Man.!! No events found</p> <a href="./index.html"><img src="./assets/img/reset.png" alt="reset"> </a></div> 
+    <img src="./assets/img/error.webp" alt="error"> </div>`;
+  }
+});
+
+
+
+
+
+//check
+const categories = [...new Set(data.events.map(event => event.category))];
+
+const filtersContainer = document.querySelector(".categorys");
+
+categories.forEach(category => {
+  const filter = document.createElement("input");
+  filter.setAttribute("type", "checkbox");
+  filter.setAttribute("id", category);
+  filter.setAttribute("value", category);
+
+  const label = document.createElement("label");
+  label.setAttribute("for", category);
+  label.innerText = category;
+
+  filtersContainer.appendChild(filter);
+  filtersContainer.appendChild(label);
+
+  filter.addEventListener("change", () => {
+    const selectedCategories = Array.from(filtersContainer.querySelectorAll("input:checked"))
+      .map(checkbox => checkbox.value);
+
+    const cards = document.querySelectorAll(".card");
+    let hay = false;
+
+    cards.forEach(card => {
+      if (selectedCategories.includes(card.getAttribute("data-type"))) {
+        hay = true;
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+
+    if (!hay) {
+      container.innerHTML = `<div class="error"> <div class="p-reset"><p class="text-center">Sorry Man.!! No events found</p> <a href="./index.html"><img src="./assets/img/reset.png" alt="reset"> </a></div>
+      <img src="./assets/img/error.webp" alt="error"> </div>`;
+    }
+  });
+});
+
 
--- assets/js/up-events.js
+++ assets/js/up-events.js
@@ -6,8 +6,8 @@ const filterEvents = eventInfo.filter(event => event.date > currentDate);
 
 const cards = filterEvents.map(recInfo => {
   return `
-    <div class="card m-2" style="width: 10rem;">
-      <img src="${recInfo.image}" class="card-img-top" alt="Festival of the collectivities">
+    <div class="card m-2" style="width: 13rem;">
+      <img src="${recInfo.image}" class="card-img-top" alt="Festival of the collectivities" style="height: 7rem;">
       <div class="card-body">
         <h5 class="card-title">${recInfo.name}</h5>
         <p class="card-text">${recInfo.date}</p>
@@ -21,3 +21,86 @@ const cards = filterEvents.map(recInfo => {
 }).join("");
 
 container.innerHTML += cards;
+
+//search
+const searchInput = document.querySelector(".form-control");
+searchInput.addEventListener("input", e => {
+  const search = e.target.value.toLowerCase();
+  const cards = document.querySelectorAll(".card");
+
+  let hay = false;
+
+  cards.forEach(card => {
+    const name = card.querySelector(".card-title").innerText.toLowerCase();
+    if (name.startsWith(search)) {
+      hay = true;
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+
+  if (!hay) {
+    container.innerHTML = `<div class="error"> <div class="p-reset"><p class="text-center">Sorry Man.!! No events found</p> <a href="./index.html"><img src="./assets/img/reset.png" alt="reset"> </a></div> 
+    <img src="./assets/img/error.webp" alt="error"> </div>`;
+  }
+});
+
+
+
+
+//check
+const categories = [...new Set(eventInfo.map(event => event.category))];
+
+const filtersContainer = document.querySelector(".categorys");
+
+categories.forEach(category => {
+  const filter = document.createElement("input");
+  filter.setAttribute("type", "checkbox");
+  filter.setAttribute("id", category);
+  filter.setAttribute("value", category);
+
+  const label = document.createElement("label");
+  label.setAttribute("for", category);
+  label.innerText = category;
+
+  filtersContainer.appendChild(filter);
+  filtersContainer.appendChild(label);
+
+  filter.addEventListener("change", () => {
+    const selectedCategories = Array.from(filtersContainer.querySelectorAll("input:checked"))
+      .map(checkbox => checkbox.value);
+
+    let hay = false;
+    const filterEvents = eventInfo.filter(event => {
+      if (selectedCategories.includes(event.category) && event.date < currentDate) {
+        hay = true;
+        return true;
+      }
+      return false;
+    });
+
+    const cards = filterEvents.map(recInfo => {
+      return `
+        <div class="card m-2" style="width: 13rem;">
+          <img src="${recInfo.image}" class="card-img-top" alt="Festival of the collectivities" style="height: 7rem;">
+          <div class="card-body">
+            <h5 class="card-title">${recInfo.name}</h5>
+            <p class="card-text">${recInfo.date}</p>
+            <div class="d-flex justify-content-around">
+              <p class="card-price">$${recInfo.price}</p>
+              <a href="./descripcion.html" class="btn text-light">More</a>
+            </div>
+          </div>
+        </div>
+      `;
+    }).join("");
+
+    container.innerHTML = cards;
+
+    if (!hay) {
+      container.innerHTML = `<div class="error"> <div class="p-reset"><p class="text-center">Sorry Man.!! No events found</p> <a href="./upcoming.html"><img src="./assets/img/reset.png" alt="reset"> </a></div>
+      <img src="./assets/img/error.webp" alt="error"> </div>`;
+    }
+  });
+});
